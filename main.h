1ca4645713d37bd92db4aea8c7debbd9d532f973
Genius-Excel <geniusexcel9@gmail.com>
2023-10-14 13:17:48 -0700

    This file contains all the function prototypes.

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+void print_integer(int val);
+int _putchar(char c);
+int _fprintf(FILE *stream, const char *format, ...);
+int _integer_length(int integer);
+
+#endif
